64f38f64ac09af3fb97e044da2db3498f73b1212
David Oligney <davidoligney@gmail.com>
2021-04-17 14:59:35 -0600

    class work

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,6 @@
 var courses = [
     {
-        title: "Raspberr Cake",
+        title: "Raspberry Cake",
         cost: 50
     },
     {
@@ -12,20 +12,8 @@ var courses = [
         cost: 100
     },
 ]
-
-//call back functions for different routes
-exports.showCourses = (req, res) => {
-    res.render("courses", {offeredCourses: courses});
-}
-
-exports.showSignUp = (req, res) => {
-    res.render("contact")
-}
-
-exports.postedSignUpForm = (req, res) => {
-    res.render("thanks")
-}
-
-exports.showIndex = (req, res) => {
-    res.render("index");
+module.exports = {
+    index: (req, res) => {
+        res.render("index");
+    }
 }
